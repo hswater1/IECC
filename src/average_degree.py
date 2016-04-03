2c1f2170e7db52c269cb9752166c613897019827
hswater1 <hswater3000@gmail.com>
2016-04-03 14:39:39 -0700

    This is the first version of my submit
    The main flow is finished, but need some checks.

--- src/average_degree.py
+++ src/average_degree.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#df_tt = pd.read_json('C:\cygwin64\home\Gordon\IECC\data-gen\teewts.txt')[:10]
 
 import json  
 
@@ -9,8 +8,20 @@ from datetime import datetime
 from datetime import timedelta
 import bisect
 import itertools
+import sys
+import os
 
-            
+
+
+if len(sys.argv) > 1:
+    tweets_data_path = sys.argv[1]
+    if len(sys.argv) > 2:
+        output_file = sys.argv[2]
+    else:
+        output_file = './tweet_output/output.txt'
+else:
+    tweets_data_path = './tweet_input/tweets.txt'
+
 def add_twt(time, hashtags):
 #to add a new tweet to the graph
     
@@ -66,8 +77,6 @@ def evict_twt(new_time, win):
 
 
 
-#Use given tweets file
-tweets_data_path = 'C:/cygwin64/home/Gordon/IECC/data-gen/tweets.txt'
 
 #Assumptions 1 - I will not use current time to decide the boundry of the 60s windows,
 #so this program can process historic tweets.
@@ -112,75 +121,11 @@ for line in tweets_file:
                 if len(new_twt_ht) > 1:
                     add_twt(new_twt_time, new_twt_ht)
                     r_avg_deg[-1] = cal_avg_deg()
-            
-           
+if r_avg_deg != None and len(r_avg_deg) > 0:  
+    with open(output_file, 'w') as f:
+        for s in r_avg_deg:
+            f.write('%.2f' % s + '\n')
+else:
+    print('No tweet has been processed')
 
 
-
-
-
-
-
-##############################################    
-'''twt_window = evict_twt(datetime.strptime('Tue Mar 29 00:04:51 +0000 2016','%a %b %d %H:%M:%S +0000 %Y'),twt_window)
-    
-pairs = []
-for twt in twt_window:
-    for p in twt[1]:
-        pairs.append(p)
-pairs.sort()
-
-uq_pairs = list(pairs for pairs,_ in itertools.groupby(pairs))
-
-edges = len(uq_pairs)
-
-nodes = [ y for x in uq_pairs for y in x]
-nodes.sort()
-uq_nodes = list(nodes for nodes,_ in itertools.groupby(nodes))
-
-
-avg_deg = edges/uq_nodes
-      
-def add_pairs(pairs):
-#Create network graph with pairs. When incr=1, add pairs. When incr = -1, drop pairs.
-
-    for pair in pairs:
-        ht_1 = [h[0] for h in twt_graph]
-        l1 = len(ht_1)
-        idx_1_r = bisect.bisect_right(ht_1, pair[0], lo=0, hi=l1)
-        idx_1_l = bisect.bisect_left(ht_1, pair[0], lo=0, hi=l1)
-        if idx_1_r == idx_1_l:
-            idx = idx_1_l
-            twt_graph.insert(idx,pair)
-        else:
-            l2 = idx_1_r - idx_1_l
-            ht_2 = [h[1] for h in twt_graph[idx_1_l:idx_1_r]]
-            idx_2 = bisect.bisect_left(ht_2, pair[1], lo=0, hi=l2)
-            idx = idx_1_l + idx_2
-            twt_graph.insert(idx, pair)
-    return
-
-
-
-twt_u_p = list(set(twt_graph))
-
-#p = gen_pairs(new_twt_ht)
-ppp = [['b','c'], ['c', 'b'],['a','c'],['e','b'],['a', 'c']]
-q= list(p for p in ppp)
-set(ppp)
-twt_graph=[]
-dd= add_pairs(ppp)
-twt_graph.append([p] for p in ppp)
-twt_graph
-xxx(ppp)
-def xxx(qqq):
-    for q in qqq:
-        twt_graph.append(q)
-    return
-
-ppp = [t[0] for t in p]
-ppp.insert(1,'c')
-bisect.bisect_right(ppp,'b',lo=0,hi=5)
-bisect.bisect_left(ppp,'b',lo=0,hi=5)     
-np_tweets = np.array(tweets_data)
-ppp.append(ppp[-1])'''
